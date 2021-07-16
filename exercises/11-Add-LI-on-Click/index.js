317e89e0e671dd75af6ab66f66b7deed4f139931
Pedro Yanez <pedro_y91@outlook.com>
2021-07-16 21:17:12 +0000

    solo falta el 13 y 14

--- exercises/11-Add-LI-on-Click/index.js
+++ exercises/11-Add-LI-on-Click/index.js
@@ -7,4 +7,21 @@ button.addEventListener("click", function() {
 	let nuevalista = document.createElement("li");
 	nuevalista.innerHTML = "Fourth Element";
 	document.querySelector("#myList").appendChild(nuevalista);
+
+});
+
+
+import { createBrotliDecompress } from "zlib";
+
+let button = document.querySelector("#superDuperButton");
+button.addEventListener("click", function() {
+	//your code here
+
+	//const expected = 'innerHTML = "Fourth element";';
+    //document.querySelector("#myList").appendChild(nuevalista);
+	//document.querySelector("#myList").appendChild((nuevalista.innerHTML = "Fourth Element"));
+
+	let nuevalista = document.createElement("li");
+	nuevalista.innerHTML = "Fourth element";
+	document.querySelector("#myList").appendChild(nuevalista);
 });
--- exercises/13-Add-Options-to-the-Select/index.js
+++ exercises/13-Add-Options-to-the-Select/index.js
@@ -2,8 +2,8 @@ window.onload = function() {
 	console.log("The website was loaded...");
 	let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
 
-    // your code here
-    let nuevalista = document.createElement("li");
+	// your code here
+	let nuevalista = document.createElement("li");
 	nuevalista.innerHTML = "Fourth Element";
 	document.querySelector("body").appendChild(nuevalista);
 };
