32d43eb681253072f0f835aff44825b80e6ae8f5
Pedro Yanez <pedro_y91@outlook.com>
2021-07-17 23:59:19 +0000

    solo falta ejercicio 14

--- exercises/13-Add-Options-to-the-Select/index.js
+++ exercises/13-Add-Options-to-the-Select/index.js
@@ -4,29 +4,15 @@ window.onload = function() {
 	// your code here
 	//let aux = document.querySelector("#mySelect");
 	let aux = document.querySelector("#mySelect"); //importante
-	let option = document.createElement("option"); //importante
+
 	for (let i = 0; i < countries.length; i++) {
+		let option = document.createElement("option"); //importante
 		option.text = countries[i];
 		aux.add(option);
+		document.querySelector("#mySelect").appendChild(option); //importante
 	}
-
-	document.querySelector("#mySelect").appendChild(option); //importante
-
-	// Rutina para agregar opciones a un <select>
-	/*
-	function addOptions(domElement, array) {
-		var select = document.getElementsByName(domElement)[0];
-
-		for (value in array) {
-			var option = document.createElement("option");
-			option.text = array[value];
-			select.add(option);
-		}
-	}*/
-
-	/*
-	let nuevalista = document.createElement("li");
-	nuevalista.innerHTML = "Fourth Element";
-    document.querySelector("body").appendChild(nuevalista);
-    */
+	aux.addEventListener("change", function() {
+		//alert(`${event.target.value}`);
+		alert(event.target.value);
+	});
 };
